a587f6264e1ab2b3edf7ee4778d150b672e95ea5
Ombre <oliverthek@gmail.com>
2023-12-28 17:12:45 +0100

    Build the NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+import { AiFillPrinter } from "react-icons/ai";
+
+const NavBar = () => {
+  const links = [
+    { label: "Bérletek", href: "/" },
+    { label: "Ügyfelek", href: "/clients" },
+    { label: "Printerek", href: "/printers" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillPrinter />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+            <Link
+              key={link.href}
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
