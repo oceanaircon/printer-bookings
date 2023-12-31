4622361613f986b8506d7f54f0997169a49b680d
Ombre <oliverthek@gmail.com>
2023-12-31 15:47:43 +0100

    authentication by next-auth and middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import { Bookings } from "./bookings";
+import { LoginButton } from "./auth";
+import { LogoutButton } from "./auth";
 
 export default function Home() {
   return (
     <div>
+      <LoginButton></LoginButton>
+      <LogoutButton></LogoutButton>
       <h2>Kliens hívás</h2>
       <Bookings></Bookings>
     </div>
