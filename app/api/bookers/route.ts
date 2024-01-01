965352c0cc2b5d315e53bf910f1d3b2381a6c8f2
Ombre <oliverthek@gmail.com>
2024-01-01 01:12:38 +0100

    prisma schema model update

--- app/api/bookers/route.ts
+++ app/api/bookers/route.ts
@@ -6,7 +6,7 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createBookerSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.format(), { status: 400 });
+    return NextResponse.json(validation.error.errors, { status: 400 });
 
   const newBooker = await prisma.booker.create({
     data: { name: body.name },
