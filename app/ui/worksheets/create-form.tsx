eaacf3941b3912ac34854a1646d1920d0f52d28a
Ombre <oliverthek@gmail.com>
2024-01-09 15:37:26 +0100

    create munkalap default FOLYMATBAN

--- app/ui/worksheets/create-form.tsx
+++ app/ui/worksheets/create-form.tsx
@@ -47,6 +47,7 @@ export default function Form({
                   name="status"
                   type="radio"
                   value="FOLYAMATBAN"
+                  checked
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
