446d8ebb78bb14e029a5a48a3f8dbfb3fb23f011
Ombre <oliverthek@gmail.com>
2024-01-30 17:56:12 +0100

    search added

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,58 +1,33 @@
 import React from "react";
-import { NewWorksheetButton, UpdateBooking } from "../ui/buttons";
 import Link from "next/link";
-import prisma from "@/prisma/client";
-import { DeleteBooking } from "../ui/deletebuttons";
+import Search from "../ui/search";
+import { Suspense } from "react";
+import BookingsTable from "../ui/bookings/bookingsTable";
 
-const BookingsPage = async () => {
-  const bookings = await prisma.booking.findMany({
-    include: {
-      booker: { select: { name: true, email: true } },
-      printer: { include: { category: { select: { fee: true } } } },
-    },
-  });
+const BookingsPage = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
 
   return (
     <div>
       <Link href="/bookings/new">
         <p>Új szerződés</p>
       </Link>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Név</th>
-            <th>Email</th>
-            <th>Printer</th>
-            <th>Serial</th>
-            <th>Díj</th>
-            <th>Start</th>
-            <th>Kedvezmény</th>
-            <th>Munkalap</th>
-          </tr>
-        </thead>
-        <tbody>
-          {bookings.map((booking) => (
-            <tr key={booking.id}>
-              <th>{booking.id}</th>
-              <th>{booking.booker.name}</th>
-              <td>{booking.booker.email}</td>
-              <td>{booking.printer.name}</td>
-              <td>{booking.printer.serial}</td>
-              <td>{booking.printer.category.fee}</td>
-              <td>{booking.createdAt.toString().slice(0, 16)}</td>
-              <td>{booking.discount}</td>
-              <td>
-                <NewWorksheetButton id={booking.id} />
-              </td>
-              <td>
-                <UpdateBooking id={booking.id}></UpdateBooking>
-                <DeleteBooking id={booking.id}></DeleteBooking>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <br />
+
+      <Search placeholder="Szerződés kereső..." />
+      <br />
+      <br />
+      <Suspense key={query + currentPage} fallback={""}>
+        <BookingsTable query={query} currentPage={currentPage} />
+      </Suspense>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import BookingsPage from "./bookings/page";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       <BookingsPage></BookingsPage>
