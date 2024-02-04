c84c3c856769392d7c620808657cde3f71f04d26
Ombre <oliverthek@gmail.com>
2024-02-04 13:31:38 +0100

    status bug corrected, auto status update added to worksheet page

--- app/ui/worksheets/create-form.tsx
+++ app/ui/worksheets/create-form.tsx
@@ -66,7 +66,7 @@ export default function Form({
                 />
                 <label
                   htmlFor="BEFEJEZETT"
-                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-gray-600"
                 >
                   BEFEJEZETT {/*<CheckIcon className="h-4 w-4" />*/}
                 </label>
